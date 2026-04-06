8b3d61dd546b0d7102de1c87fb7add2634ee4fb7
kaungsan2001 <kaungsan2001.dev@gmail.com>
2026-04-06 22:15:56 +0630

    initial setup

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
+import cors from "cors";
+const app = express();
+const port = 8000;
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
+
+app.all("/api/auth/{*any}", toNodeHandler(auth));
+
+app.use(express.json());
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
